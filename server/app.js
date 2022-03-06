1b084b666c88c28b89c3dbe6c1127026fe812417
Aryaman <jainaryaman123@gmail.com>
2022-03-06 14:46:56 +0530

    api np without tests

---
+++ server/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const cors = require('cors');
+const morgan = require('morgan');
+const translateRoute = require('./routes/translate');
+const { errorMonitor } = require('events');
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use('/',translateRoute);
+
+
+//ask this
+app.use((req,res,next) => {
+    const error = new Error('Not Found!');
+    error.status = 400;
+    next(error);
+});
+app.use((error, req, res, next) => {
+    res.status(error.status || 500).json({
+        error: {
+            message: error.message,
+        }
+    })
+});
+module.exports = app;
---
+++ server/routes/translate.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cache = require('../translateCache');
+const router = express.Router();
+const googleTranslate = require('@vitalets/google-translate-api');
+
+
+router.get('/translate',cache(10),async(req,res,next)=>{
+    var queryResponse = {}
+    try{
+        const answer = await googleTranslate(req.query.sourceText, {to: req.query.targetLanguage});
+
+        queryResponse.finalText = answer.text;
+        queryResponse.from = answer.from.language;
+    } catch (error)
+    {
+        console.log(error);
+    }
+
+    res.status(200).json({
+        success: true,
+        data: queryResponse,
+    })
+});
+
+module.exports = router;
